31843619d11d0c34836c09baa9f1c89d445f5d30
MusabBinAhsan <yosinger13@gmail.com>
2024-09-15 12:25:29 +0500

    added script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+alert("Hackers Alert Are You Aware Click Ok");
+
+x = 5;
+y = 6;
+z = x + y;
+
+console.log(z)
+x = "Welcome "
+y = "Hahaaaha";
+z = x + y;
+
+console.log(z)
+a = 50;
+b = 90;
+c = a + b; 
+console.log(c);
+
+var a = 60;
+var b = 40;
+var c = a - b;
+console.log (c)
+
+var weight = 100;
+weight = weight * 50; 
+console.log (weight)
+
+// legal variable In Js:
+
+let Name = 'Musab';
+let Surname = 'Ahsan';
+let surname = Name + Lastname;
+console.log(surname)
+
+let y_ear = 2024;
+var age = 20;
+let add2 = y_ear - age;
+console.log(add2)
+
+let amount$ = 2000;
+let Discount = 300;
+let price = amount$ - Discount;
+console.log(price)  
+
+// // Illegal Variable In js:
+
+// let my name = 'Musab';
+// var = 'Ahsan';
+// var else = my name + var;
+// console.log(var)
+
+// let 2number = 78;
+// let function = 87;
+// let my-num = 2number + function;
+// console.log(my-num)
